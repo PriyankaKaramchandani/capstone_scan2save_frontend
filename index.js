e9cdef8c3df02d65445d2b6b887b147fd99e8ace
Priyanka <pri.ppk@gmail.com>
2025-01-24 14:56:06 -0800

    Remove files tracked by Git that are now ignored

--- index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-console.log('App Name:', appName); // Debug log
-AppRegistry.registerComponent(appName, () => App);
